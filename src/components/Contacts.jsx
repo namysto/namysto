6d5529bea1b6ff224a1dab5599ea7a20eb317a7c
Alina <a.08pon@gmail.com>
2024-01-24 17:01:27 +0000

    minor changes

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,9 +5,9 @@ const Contacts = () => {
       <p>
         For inquiries, collaboration opportunities, or any assistance you may
         need, we invite you to reach out to us. You can contact us via email at
-        [your@email.com], and our team will be delighted to respond to your
-        queries promptly. We value open communication and look forward to
-        hearing from you, whether you are seeking information about our
+        ukrainianschoolinuk@gmail.com, and our team will be delighted to respond
+        to your queries promptly. We value open communication and look forward
+        to hearing from you, whether you are seeking information about our
         projects, wish to volunteer, or explore potential partnerships. Your
         engagement is vital to the success of our initiatives, and we are eager
         to connect with you.
